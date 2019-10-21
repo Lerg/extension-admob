c936a183bc6cfba0ab0c5a9beb7b64a3ac1ada5a
Lerg <sergey.lerg@gmail.com>
2019-10-21 14:49:40 +0500

    Updated for iOS 13.

--- admob/lib/ios/GoogleMobileAds.framework/Headers/GADNativeExpressAdView.h
+++ admob/lib/ios/GoogleMobileAds.framework/Headers/GADNativeExpressAdView.h
@@ -2,18 +2,15 @@
 //  GADNativeExpressAdView.h
 //  Google Mobile Ads SDK
 //
-//  Copyright 2015 Google Inc. All rights reserved.
+//  Copyright 2015 Google LLC. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
 
 #import <GoogleMobileAds/GADAdSize.h>
 #import <GoogleMobileAds/GADNativeExpressAdViewDelegate.h>
 #import <GoogleMobileAds/GADRequest.h>
 #import <GoogleMobileAds/GADVideoController.h>
 #import <GoogleMobileAds/GoogleMobileAdsDefines.h>
-
-GAD_ASSUME_NONNULL_BEGIN
+#import <UIKit/UIKit.h>
 
 /// The view that displays native ads. A minimum implementation to get an ad from within a
 /// UIViewController class is:
@@ -29,20 +26,21 @@ GAD_ASSUME_NONNULL_BEGIN
 ///   // Request an ad without any additional targeting information.
 ///   [adView loadRequest:[GADRequest request]];
 ///   </pre>
+GAD_DEPRECATED_ATTRIBUTE
 @interface GADNativeExpressAdView : UIView
 
 #pragma mark - Initialization
 
 /// Returns an initialized GADNativeExpressAdView instance set to |adSize| and positioned at
 /// |origin| relative to its superview bounds. Returns nil if |adSize| is an invalid ad size.
-- (instancetype GAD_NULLABLE_TYPE)initWithAdSize:(GADAdSize)adSize origin:(CGPoint)origin;
+- (nullable instancetype)initWithAdSize:(GADAdSize)adSize origin:(CGPoint)origin;
 
-/// Returns an initialized GADNativeExpressAdView instance set to |adSize| and positioned at the top
-/// left of its superview. Returns nil if |adSize| is an invalid ad size.
-- (instancetype GAD_NULLABLE_TYPE)initWithAdSize:(GADAdSize)adSize;
+/// Returns an initialized GADNativeExpressAdView instance set to |adSize| and positioned at the
+/// top left of its superview. Returns nil if |adSize| is an invalid ad size.
+- (nullable instancetype)initWithAdSize:(GADAdSize)adSize;
 
 /// Video controller for controlling video rendered by this native express ad view.
-@property(nonatomic, strong, readonly) GADVideoController *videoController;
+@property(nonatomic, readonly, nonnull) GADVideoController *videoController;
 
 #pragma mark - Pre-Request
 
@@ -51,11 +49,11 @@ GAD_ASSUME_NONNULL_BEGIN
 /// important for targeting and statistics.
 ///
 /// Example AdMob ad unit ID: @"ca-app-pub-0123456789012345/0123456789"
-@property(nonatomic, copy, GAD_NULLABLE) IBInspectable NSString *adUnitID;
+@property(nonatomic, copy, nullable) IBInspectable NSString *adUnitID;
 
 /// Required reference to the current root view controller. For example, the root view controller in
 /// a tab-based application would be the UITabViewController.
-@property(nonatomic, weak, GAD_NULLABLE) IBOutlet UIViewController *rootViewController;
+@property(nonatomic, weak, nullable) IBOutlet UIViewController *rootViewController;
 
 /// Required to set this native ad view to a proper size. Never create your own GADAdSize directly.
 /// Use one of the predefined standard ad sizes (such as kGADAdSizeBanner), or create one using the
@@ -66,7 +64,7 @@ GAD_ASSUME_NONNULL_BEGIN
 
 /// Optional delegate object that receives state change notifications from this
 /// GADNativeExpressAdView. Typically this is a UIViewController.
-@property(nonatomic, weak, GAD_NULLABLE) IBOutlet id<GADNativeExpressAdViewDelegate> delegate;
+@property(nonatomic, weak, nullable) IBOutlet id<GADNativeExpressAdViewDelegate> delegate;
 
 /// A Boolean value that determines whether autoloading of ads in the receiver is enabled. If
 /// enabled, you do not need to call the loadRequest: method to load ads.
@@ -76,21 +74,19 @@ GAD_ASSUME_NONNULL_BEGIN
 ///
 /// @param adOptions An array of GADAdLoaderOptions objects. The array is deep copied and option
 /// objects cannot be modified after calling this method.
-- (void)setAdOptions:(NSArray *)adOptions;
+- (void)setAdOptions:(nonnull NSArray *)adOptions;
 
 #pragma mark - Making an Ad Request
 
 /// Makes an ad request. The request object supplies targeting information.
-- (void)loadRequest:(GADRequest *GAD_NULLABLE_TYPE)request;
+- (void)loadRequest:(nullable GADRequest *)request;
 
 #pragma mark - Mediation
 
 /// The name of the ad network adapter class that fetched the current ad. Returns nil while the
 /// latest ad request is in progress or if the latest ad request failed. For both standard and
-/// mediated Google AdMob ads, this method returns @"GADMAdapterGoogleAdMobAds". For ads fetched via
-/// mediation custom events, this method returns @"GADMAdapterCustomEvents".
-@property(nonatomic, readonly, weak, GAD_NULLABLE) NSString *adNetworkClassName;
+/// mediated Google AdMob ads, this method returns @"GADMAdapterGoogleAdMobAds". For ads fetched
+/// via mediation custom events, this method returns @"GADMAdapterCustomEvents".
+@property(nonatomic, readonly, weak, nullable) NSString *adNetworkClassName;
 
 @end
-
-GAD_ASSUME_NONNULL_END
--- admob/lib/ios/GoogleMobileAds.framework/Headers/GADSearchRequest.h
+++ admob/lib/ios/GoogleMobileAds.framework/Headers/GADSearchRequest.h
@@ -2,16 +2,13 @@
 //  GADSearchRequest.h
 //  Google Mobile Ads SDK
 //
-//  Copyright 2011 Google Inc. All rights reserved.
+//  Copyright 2011 Google LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
 #import <GoogleMobileAds/GADRequest.h>
 #import <GoogleMobileAds/GoogleMobileAdsDefines.h>
-
-GAD_ASSUME_NONNULL_BEGIN
+#import <UIKit/UIKit.h>
 
 /// Search ad border types.
 typedef NS_ENUM(NSUInteger, GADSearchBorderType) {
@@ -32,31 +29,31 @@ typedef NS_ENUM(NSUInteger, GADSearchCallButtonColor) {
 @interface GADSearchRequest : GADRequest
 
 /// The search ad query.
-@property(nonatomic, copy, GAD_NULLABLE) NSString *query;
+@property(nonatomic, copy, nullable) NSString *query;
 /// The search ad background color.
-@property(nonatomic, readonly, copy, GAD_NULLABLE) UIColor *backgroundColor;
+@property(nonatomic, readonly, copy, nullable) UIColor *backgroundColor;
 /// The search ad gradient "from" color.
-@property(nonatomic, readonly, copy, GAD_NULLABLE) UIColor *gradientFrom;
+@property(nonatomic, readonly, copy, nullable) UIColor *gradientFrom;
 /// The search ad gradient "to" color.
-@property(nonatomic, readonly, copy, GAD_NULLABLE) UIColor *gradientTo;
+@property(nonatomic, readonly, copy, nullable) UIColor *gradientTo;
 /// The search ad header color.
-@property(nonatomic, copy, GAD_NULLABLE) UIColor *headerColor;
+@property(nonatomic, copy, nullable) UIColor *headerColor;
 /// The search ad description text color.
-@property(nonatomic, copy, GAD_NULLABLE) UIColor *descriptionTextColor;
+@property(nonatomic, copy, nullable) UIColor *descriptionTextColor;
 /// The search ad anchor text color.
-@property(nonatomic, copy, GAD_NULLABLE) UIColor *anchorTextColor;
+@property(nonatomic, copy, nullable) UIColor *anchorTextColor;
 /// The search ad text font family.
-@property(nonatomic, copy, GAD_NULLABLE) NSString *fontFamily;
+@property(nonatomic, copy, nullable) NSString *fontFamily;
 /// The search ad header text size.
 @property(nonatomic, assign) NSUInteger headerTextSize;
 /// The search ad border color.
-@property(nonatomic, copy, GAD_NULLABLE) UIColor *borderColor;
+@property(nonatomic, copy, nullable) UIColor *borderColor;
 /// The search ad border type.
 @property(nonatomic, assign) GADSearchBorderType borderType;
 /// The search ad border thickness.
 @property(nonatomic, assign) NSUInteger borderThickness;
 /// The search ad custom channels.
-@property(nonatomic, copy, GAD_NULLABLE) NSString *customChannels;
+@property(nonatomic, copy, nullable) NSString *customChannels;
 /// The search ad call button color.
 @property(nonatomic, assign) GADSearchCallButtonColor callButtonColor;
 
@@ -64,14 +61,12 @@ typedef NS_ENUM(NSUInteger, GADSearchCallButtonColor) {
 /// can either be a solid color, or a gradient, which can be specified through
 /// setBackgroundGradientFrom:toColor: method. If both solid and gradient
 /// background is requested, only the latter is considered.
-- (void)setBackgroundSolid:(UIColor *)color;
+- (void)setBackgroundSolid:(nonnull UIColor *)color;
 
 /// A linear gradient background color for rendering the ad. The background of
 /// the ad can either be a linear gradient, or a solid color, which can be
 /// specified through setBackgroundSolid method. If both solid and gradient
 /// background is requested, only the latter is considered.
-- (void)setBackgroundGradientFrom:(UIColor *)from toColor:(UIColor *)toColor;
+- (void)setBackgroundGradientFrom:(nonnull UIColor *)from toColor:(nonnull UIColor *)toColor;
 
 @end
-
-GAD_ASSUME_NONNULL_END
